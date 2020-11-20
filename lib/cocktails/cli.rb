fc434d687aed06145107d387dde32cf1bf70c496
Oroko <aoroko09@gmail.com>
2020-11-19 22:13:06 -0500

    adds a print_drinks method to list all the drinks

--- lib/cocktails/cli.rb
+++ lib/cocktails/cli.rb
@@ -2,7 +2,6 @@
 
 module Cocktails
   class CLI
-
     def run
       puts ''
       puts 'Welcome to the CLI Bar'
@@ -11,14 +10,14 @@ module Cocktails
       puts 'Enter an ingredient to see drink recipes that use it or "exit" to exit.'
       puts ''
       @ingredient = gets.strip.downcase
-
       Cocktails::API.fetch_drinks(@ingredient)
+      print_drinks
+    end
 
-      drinks = Cocktails::Drink.all
-      drinks.each.with_index(1) do |d, i|
+    def print_drinks
+      Cocktails::Drink.all.each.with_index(1) do |d, i|
         puts "#{i}. #{d.name}"
       end
     end
-
   end
 end
