6f63d233346cfa53bc37cbe0b3ed564eab609826
Oroko <aoroko09@gmail.com>
2020-11-19 22:08:19 -0500

    create my model class that collects all the info of all the objects and returns it, also creates a helper method find_by_ingredient

--- lib/cocktails/drink.rb
+++ lib/cocktails/drink.rb
@@ -2,14 +2,23 @@
 
 module Cocktails
   class Drink
+    attr_accessor :name, :drink_id, :ingredient
+
     @@all = []
 
-    def initialize
+    def initialize(name:, ingredient:, drink_id:)
+      @name = name
+      @ingredient = ingredient
+      @drink_id = drink_id
       @@all << self
     end
 
     def self.all
       @@all
+    end
+    # helper class method
+    def self.find_by_ingredient(ingredient)
+      all.select { |d| d.ingredient == ingredient }
     end
   end
 end
