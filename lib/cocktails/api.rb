7175a5dd3e264c20b09f3e9da95cafdf9f6d7683
Oroko <aoroko09@gmail.com>
2020-11-19 22:03:06 -0500

    add a class method to fetch data from api and initialize the class with the data from the api

--- lib/cocktails/api.rb
+++ lib/cocktails/api.rb
@@ -2,6 +2,16 @@
 
 module Cocktails
   class API
-    
+    def self.fetch_drinks(ingredient)
+      url = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=#{ingredient}"
+      response = HTTParty.get(url)
+      drinks = response['drinks'].each do |c|
+        unless c['strDrink'].nil?
+          Cocktails::Drink.new(name: c['strDrink'], drink_id: c['idDrink'],
+                               ingredient: ingredient)
+        end
+      end
+      #binding.pry
+    end
   end
 end
